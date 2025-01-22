1715d75ac97450d50f97db34e59a61b0a0c8e451
UnkownFate <jxst991059@outlook.com>
2025-01-22 22:04:06 +0800

    Add files via upload

---
+++ Docker/RSSHub/RSSHub.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# DOCKER_RUN_FILE of docker-tool.sh V1.0.0
+
+# Copyleft © 2024-2025
+# https://github.com/UnkownFate/docker-tool.sh
+# Last updated: 2025-01-22
+
+# This work is licensed under GNU AGPL 3.0.
+# https://github.com/UnkownFate/docker-tool.sh?tab=AGPL-3.0-1-ov-file
+
+function docker_run() {
+
+echo ""
+set -x
+
+docker run --name $DOCKER_PROJECT_NAME \
+  --restart always \
+  -d \
+  -p 1200:1200 \
+  -v /etc/localtime:/etc/localtime:ro \
+  -e CACHE_EXPIRE=3600 \
+  diygod/rsshub:chromium-bundled
+
+{ set +x; } >/dev/null 2>&1
+
+}
