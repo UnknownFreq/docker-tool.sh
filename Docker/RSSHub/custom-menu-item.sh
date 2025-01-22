03f245bb4c538e184972b7c5610cadd26bbbe773
UnkownFate <jxst991059@outlook.com>
2025-01-22 22:03:39 +0800

    custom-menu-item.sh

---
+++ Docker/RSSHub/custom-menu-item.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# custom-menu-item of docker-tool.sh V1.0.0
+
+# Copyleft © 2024-2025
+# https://github.com/UnkownFate/docker-tool.sh
+# Last updated: 2025-01-22
+
+# This work is licensed under GNU AGPL 3.0.
+# https://github.com/UnkownFate/docker-tool.sh?tab=AGPL-3.0-1-ov-file
+
+# 取消声明不需要的菜单变量
+unset -v project_menu_text_1 project_menu_command_1
+unset -v project_menu_text_2 project_menu_command_2
+unset -v project_menu_text_6 project_menu_command_6
